febab1fe33652e81fa29d51436dfcea3561cc0a7
iriebijudicael <iriejudicaelelvis225@gmail.com>
2025-11-29 00:24:35 +0000

    Files edited

--- scripts/form.js
+++ scripts/form.js
@@ -77,3 +77,9 @@ function displayErrors(errorList) {
         </ul>
     `;
 }
+
+// Dynamically populate the current year in the footer
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Dynamically populate the last modified date
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
